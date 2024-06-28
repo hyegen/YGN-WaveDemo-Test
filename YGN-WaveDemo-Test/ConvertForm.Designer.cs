230c3f52d8e8efeea8c77f3d5be89b79e47094e1
Hüseyin <hyegen205@gmail.com>
2024-06-28 22:53:01 +0300

    Add project files.

---
+++ YGN-WaveDemo-Test/ConvertForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace YGN_WaveDemo_Test
+{
+    partial class ConvertForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnSelectWaveFile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 76);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1367, 587);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // btnSelectWaveFile
+            // 
+            this.btnSelectWaveFile.Location = new System.Drawing.Point(475, 12);
+            this.btnSelectWaveFile.Name = "btnSelectWaveFile";
+            this.btnSelectWaveFile.Size = new System.Drawing.Size(107, 35);
+            this.btnSelectWaveFile.TabIndex = 1;
+            this.btnSelectWaveFile.Text = "Dosya Seç";
+            this.btnSelectWaveFile.UseVisualStyleBackColor = true;
+            this.btnSelectWaveFile.Click += new System.EventHandler(this.btnSelectWaveFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1399, 676);
+            this.Controls.Add(this.btnSelectWaveFile);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btnSelectWaveFile;
+    }
+}
+
---
+++ YGN-WaveDemo-Test/ConvertForm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.Dsp;
+using System.Windows.Forms.DataVisualization.Charting;
+using MathNet.Numerics.IntegralTransforms;
+
+namespace YGN_WaveDemo_Test
+{
+    public partial class ConvertForm : Form
+    {
+        public ConvertForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectWaveFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Audio Files|*.m4a;*.wav";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+
+                    AnalyzeAudioFile(filePath);
+                }
+            }
+        }
+        private void AnalyzeAudioFile(string filePath)
+        {
+            using (var audioFileReader = new AudioFileReader(filePath))
+            {
+                var sampleProvider = audioFileReader.ToSampleProvider();
+                float[] buffer = new float[audioFileReader.WaveFormat.SampleRate];
+                int samplesRead;
+
+                var fftSize = 4096;
+                var fftBuffer = new Complex[fftSize];
+                int fftPos = 0;
+                int fftFull = 0;
+
+                while ((samplesRead = sampleProvider.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    for (int n = 0; n < samplesRead; n++)
+                    {
+                        fftBuffer[fftPos].X = (float)(buffer[n] * FastFourierTransform.HannWindow(fftPos, fftSize));
+                        fftBuffer[fftPos].Y = 0;
+                        fftPos++;
+                        if (fftPos >= fftSize)
+                        {
+                            fftPos = 0;
+                            fftFull++;
+                            if (fftFull > 1)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                FastFourierTransform.FFT(true, (int)Math.Log(fftSize, 2.0), fftBuffer);
+
+               // var magnitude = fftBuffer.Select(c => Math.Sqrt(c.X * c.X + c.Y * c.Y)).Take(1500).ToArray();
+                var magnitude = fftBuffer.Select(c => Math.Sqrt(c.X * c.X + c.Y * c.Y)).Take(500).ToArray();
+                //var frequencies = Enumerable.Range(0, magnitude.Length)
+                //                            .Select(i => i * audioFileReader.WaveFormat.SampleRate / fftSize)
+                //                            .Take(1500)
+                //                            .ToArray();
+                var frequencies = Enumerable.Range(0, magnitude.Length)
+                                       .Select(i => i * audioFileReader.WaveFormat.SampleRate / fftSize)
+                                       .Take(500)
+                                       .ToArray();
+
+                chart1.Series["Series1"].Points.Clear();
+                for (int i = 0; i < frequencies.Length / 2; i++)
+                {
+                    chart1.Series["Series1"].Points.AddXY(frequencies[i], magnitude[i]);
+                }
+            }
+        }
+    }
+}
